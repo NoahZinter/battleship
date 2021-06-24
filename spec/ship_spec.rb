f02c58b7d76429bef47895af02949b621495c2ce
NoahZinter <noah.zinter@gmail.com>
2021-06-23 19:11:59 -0600

    Test ship attributes

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,9 +1,24 @@
 require './lib/ship'
 
 describe Ship do
+  before(:each) do
+    @cruiser = Ship.new('Cruiser', 3)
+  end
   it 'exists' do
-    cruiser = Ship.new('Cruiser', 3)
+    expect(@cruiser).is_a? Ship
+  end
 
-    expect(cruiser).is_a? Ship
+  describe 'attributes' do
+    it 'has a name' do
+      expect(@cruiser.name).to eq 'Cruiser'
+    end
+
+    it 'has a length' do
+      expect(@cruiser.length).to eq 3
+    end
+
+    it 'has health equal to length' do
+      expect(@cruiser.health).to eq 3
+    end
   end
 end
