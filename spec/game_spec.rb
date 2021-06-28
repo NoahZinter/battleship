864013fdec6600088037757b7af6f4167fd76b88
NoahZinter <noah.zinter@gmail.com>
2021-06-28 13:33:55 -0600

    Test initialization and initial outputs for game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,39 +2,49 @@ require './lib/board'
 require './lib/cell'
 require './lib/computer'
 require './lib/game'
-require './lib/player'
+require './lib/computer'
 require './lib/ship'
 
 describe Game do
   before(:each) do
     @game = Game.new
-    
+
   end
   describe '#initialize' do
     it 'exists' do
-      
+      expect(@game).is_a? Game
     end
 
     it 'starts with game_over of false' do
-      
+      expect(@game.game_over).to eq false
     end
 
     it 'starts with max length of 3' do
-      
+      expect(@game.max_length).to eq 3
     end
 
     it 'has a player' do
-      
+      expect(@game.player).to eq nil
+
+      @game.player_creator(4)
+
+      expect(@game.player).is_a? Player
     end
 
     it 'has a computer' do
-      
+      expect(@game.computer).to eq nil
+
+      @game.player_creator(4)
+
+      expect(@game.computer).is_a? Computer
     end
   end
 
   describe '#welcome_message' do
     it 'sends welcome to stdout' do
-      
+      expect do
+        @game.welcome_message
+      end.to output("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nWelcome to BATTLESHIP!!!!\nEnter (P) to play. Enter (Q) to quit.\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n").to_stdout
     end
   end
 
